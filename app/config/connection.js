d1524138032bec53856d918def943449a50676b8
Li Chien <lichien@Lis-MacBook-Pro.local>
2018-03-14 01:40:05 -0400

    created connection w mysql

---
+++ app/config/connection.js
@@ -0,0 +1,18 @@
+  ////////////////////////////////
+ //INITIATE CONNECTION TO MYSQL//
+////////////////////////////////
+
+var Sequelize = require("sequelize");
+
+
+var sequelize = new Sequelize("lore_db", "root", "", {
+	host: "localhost",
+	dialect: "mysql",
+	pool: {
+		max: 5,
+		min: 0,
+		idle: 10000
+	}
+});
+
+module.exports = sequelize;
