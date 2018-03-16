6a6e7744a8072cb7687f1ed3192a523d04afa181
Li Chien <lichien@Lis-MacBook-Pro.fios-router.home>
2018-03-16 16:12:05 -0400

    attached modal to sign up button

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -6,8 +6,6 @@ $( document ).ready(function() {
 
  $(document).on('click', '.trigger', function (event) {
     event.preventDefault();
-    // $('#modal').iziModal('setZindex', 99999);
-    // $('#modal').iziModal('open', { zindex: 99999 });
     $('#modal').iziModal('open');
 });
 
