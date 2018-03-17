6f6ebf37c4bd190426fccdc4963744b852461c0f
María Seneca <misat0@MISAT0.fios-router.home>
2018-03-17 09:55:57 -0400

    server structuring

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
+
+//Dependencies
+//=======================================
 var express = require('express')
 var bodyParser = require('body-parser')
 
+//Sets up Express app
+//=======================================
 var app = express()
 var PORT = process.env.PORT || 8080
 
-//create application/json parser
-var jsonParser = bodyParser.json()
+//Sets up Express app to handle data parsing
+//=======================================
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}))
 
-//create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({extended:false})
-
-//parse various different custom JSON types as JSON
-app.use(bodyParser.json({type: 'application/**json'}))
-
-//parse some custom thing into a Buffer
-app.use(bodyParser.raw({type: 'application/vnd.custom-type'}))
-
-//parse an HTML body into a starting
-app.use(bodyParser.text({type: 'text/html'}))
+//Static Directory
+app.use(express.static("app/public"));
 
 //Routes
 //=========================
 require(".app/routes/api-routes.js")(app);
 require(".app/routes/html-routes.js")(app);
 
-
+//Starts server to begin listening
+//=======================================
 app.listen(PORT, function(){
     console.log("App listening on PORT:" + PORT);
 
