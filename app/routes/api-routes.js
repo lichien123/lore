45cbacaed3e01648cad6522f6ffc728ae8ebdca2
Li Chien <lichien@Lis-MacBook-Pro.local>
2018-03-17 10:51:06 -0400

    pushing code so we all can work on the same thing. there is an error with the findAll function at api-routes.js:18

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -15,7 +15,7 @@ module.exports = function(app) {
   // GET route for getting all of the todos
   app.get("/api", function(req, res) {
     // findAll returns all entries for a table when used with no options
-    loreModel.userInfo.findAll({}).then(function(userInfoObjectModel) {
+    model.userInfo.findAll({}).then(function(userInfoObjectModel) {
       // We have access to the todos as an argument inside of the callback function
       res.json(userInfoObjectModel);
     });
