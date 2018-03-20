8b24bdb0b561fe9c9c8027592aff3b3b784696fa
DanYee92 <danyee92@gmail.com>
2018-03-20 17:56:49 -0400

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'root'
+    },
+	'database': 'my_schema',
+    'users_table': 'users'
+};
