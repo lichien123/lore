ee312d31cf7c75012704da6bf26828f81fbacf1c
María Seneca <blackopal@protonmail.com>
2018-03-24 04:33:25 -0400

    lota oops

--- app/config/database.js
+++ app/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
     'connection': {
         'host': 'localhost',
         'user': 'root',
-        'password': 'root'
+        'password': 'NEWPASS'
     },
 	'database': 'my_schema',
     'users_table': 'users'
